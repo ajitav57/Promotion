c54c598a3de087822966f436af5318c78e74cde3
ajitav57 <45916439+ajitav57@users.noreply.github.com>
2019-10-19 13:05:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 // To test commit history
 class HelloWorld {
     public static void main(String[] args) {
-System.out.println("Hello, World!"); 
+System.out.println("My first job from webhook"); 
 
 
     }
